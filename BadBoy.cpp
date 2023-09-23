7122ddfe21902560dbb149d5e191e9c0e23ba081
Arya Samik <aryasamik@gmail.com>
2023-09-24 03:35:59 +0530

    added a new code -> BadBoy.cpp

---
+++ BadBoy.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    long long n,m,i,j;
+    while(t--){
+        cin >> n >> m >> i >> j;
+        cout << "1 1 " << n << " " << m <<endl; 
+    }
+}
